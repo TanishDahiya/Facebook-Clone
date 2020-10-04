6e09ffd6e8aa41f1a054783c43d73db029e56eb9
Tanish <tanishdahiya25@outlook.com>
2020-10-04 18:54:26 +0530

    FB-Clone

---
+++ src/Sidebar.js
@@ -0,0 +1,32 @@
+import { ExpandMoreOutlined, LocalHospital, People } from '@material-ui/icons';
+import userEvent from '@testing-library/user-event';
+import React from 'react'
+import "./Sidebar.css"
+import SidebarRow from "./SidebarRow"
+import LocalHospitalIcon from "@material-ui/icons/LocalHospital";
+import EmojiFlagsIcon from "@material-ui/icons/EmojiFlags";
+import PeopleIcon from "@material-ui/icons/People";
+import ChatIcon from "@material-ui/icons/Chat";
+import StorefrontIcon from "@material-ui/icons/Storefront";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import PersonIcon from "@material-ui/icons/Person";
+
+function Sidebar() {
+    return (
+      <div className="sidebar">
+        <SidebarRow src={PersonIcon} title="Tanish Dahiya" />
+        <SidebarRow
+          Icon={LocalHospitalIcon}
+          title="COVID-19 Information Center"
+        />
+        <SidebarRow Icon={EmojiFlagsIcon} title="Pages" />
+        <SidebarRow Icon={PeopleIcon} title="Friends" />
+        <SidebarRow Icon={ChatIcon} title="Messenger" />
+        <SidebarRow Icon={StorefrontIcon} title="Marketplace" />
+        <SidebarRow Icon={VideoLibraryIcon} title="videos" />
+        <SidebarRow Icon={ExpandMoreOutlined} title="Marketplace" />
+      </div>
+    );
+}
+
+export default Sidebar
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDDi4uwlPXlbPqBXPxfINMajlZVQm0nQPs",
+    authDomain: "facebook-clone-b8d9d.firebaseapp.com",
+    databaseURL: "https://facebook-clone-b8d9d.firebaseio.com",
+    projectId: "facebook-clone-b8d9d",
+    storageBucket: "facebook-clone-b8d9d.appspot.com",
+    messagingSenderId: "201927162557",
+    appId: "1:201927162557:web:a14781b005c165fa2415d4",
+    measurementId: "G-XXH1HE6QMG"
+};
